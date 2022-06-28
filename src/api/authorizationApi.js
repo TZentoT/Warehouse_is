f4b99627daa11dc67e9a8874d00cc649cbea1114
usc5h_743pg2c <uc00510144@yandex.ru>
2022-06-28 11:04:05 +0300

    change route

--- src/api/authorizationApi.js
+++ src/api/authorizationApi.js
@@ -7,7 +7,7 @@ export class Api {
     getClients () {
         var xhr = new XMLHttpRequest();
         return new Promise(function(resolve, reject){
-            xhr.open("GET", host+"clients/", true);
+            xhr.open("GET", host+"clients", true);
             // xhr.setRequestHeader("Access-Control-Allow-Origin", "*")
             // xhr.setRequestHeader("Access-Control-Allow-Credentials", "true");
             // xhr.setRequestHeader("Access-Control-Max-Age", "1800");
